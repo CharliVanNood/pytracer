9f06224eb070ace817bf04fc77d443d7a9499411
Charli <charli.vannood@gmail.com>
2025-09-27 20:22:00 +0200

    movement

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ while running:
     #renderer.player.rotation.add(0, 1, 0)
 
     #renderer.objects[0].position.y = math.sin(frame / 10) / 5
-    #renderer.objects[1].position.y = math.sin(frame / 10) / 5
+    renderer.objects[1].position.y = math.sin(frame / 10) / 5
     #renderer.objects[2].position.y = math.sin(frame / 10) / 5
 
     renderer.render()
